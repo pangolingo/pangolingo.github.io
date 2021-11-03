62bc9d2852bc8c0d25a98caedbf43c415b4139ce
Dave Iverson <diverson@rightpoint.com>
2021-11-02 22:32:49 -0500

    Configured Prettier a little better and used it to autoformat some more files

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,16 @@
 var gulp = require('gulp'),
-    minifycss = require('gulp-minify-css'),
-    rename = require('gulp-rename');
-    // sass = require('gulp-ruby-sass'),
-    // autoprefixer = require('gulp-autoprefixer'),
+  minifycss = require('gulp-minify-css'),
+  rename = require('gulp-rename');
 
 function build() {
-  return gulp.src(['css/*.css', '!css/*.min.css'])
-    .pipe(minifycss({compatibility: 'ie8'}))
-    .pipe(rename({suffix: '.min'}))
+  return gulp
+    .src(['css/*.css', '!css/*.min.css'])
+    .pipe(minifycss({ compatibility: 'ie8' }))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('css'));
 }
 gulp.task('build', build);
 
-gulp.task('watch', function() {
-  return gulp.watch(['css/*.css', '!css/*.min.css'], build)
+gulp.task('watch', function () {
+  return gulp.watch(['css/*.css', '!css/*.min.css'], build);
 });
